ce603ae22b7d5470409c16320a482d4859582428
AndreaMinelli <minelliandrea94@gmail.com>
2023-01-24 16:28:09 +0100

    Creato background

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      pokemonUri:
+        "https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=10&page=1",
+    };
+  },
+  mounted() {
+    axios.get(this.pokemonUri).then((res) => {
+      console.log(res.data);
+    });
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <div class="container"></div>
+</template>
 
 <style lang="scss">
 @use "./assets/scss/style.scss";
