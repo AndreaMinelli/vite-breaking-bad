3a531da42efe2c44d5c40a6641c372439d20748d
AndreaMinelli <minelliandrea94@gmail.com>
2023-01-25 16:50:31 +0100

    Modificato background

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -23,33 +23,38 @@ export default {
 
 <template>
   <div class="container">
-    <div class="select-type">
-      <select-options
-        first-option="All"
-        :options="pokemonTypes"
-        class="w-25"
-        @selected-option="getPokemonType"></select-options>
-    </div>
-
-    <app-loader
-      v-if="store.isLoading"
-      image="../assets/img/pikachu-loading.jpg">
-    </app-loader>
-    <div v-else class="row row-cols-md-3 row-cols-lg-4">
-      <div class="col" v-for="pokemon in store.pokemonList" :key="pokemon.id">
-        <pokemon-card :pokemon="pokemon"></pokemon-card>
+    <div class="main-view">
+      <div class="select-type">
+        <select-options
+          first-option="All"
+          :options="pokemonTypes"
+          class="w-25"
+          @selected-option="getPokemonType"></select-options>
+      </div>
+      <app-loader
+        v-if="store.isLoading"
+        image="../assets/img/pikachu-loading.jpg">
+      </app-loader>
+      <div v-else class="row row-cols-md-3 row-cols-lg-4">
+        <div class="col" v-for="pokemon in store.pokemonList" :key="pokemon.id">
+          <pokemon-card :pokemon="pokemon"></pokemon-card>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.main-view {
+  height: 60%;
+  width: 95%;
+}
 .select-type {
   margin: 1rem 1rem 1rem auto;
   text-align: end;
 }
 .row {
-  height: 85%;
+  height: 100%;
   overflow-y: auto;
 }
 
